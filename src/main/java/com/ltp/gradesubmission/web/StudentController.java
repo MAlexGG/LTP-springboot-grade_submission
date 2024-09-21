94b531fe91831cddeec2553148103f172fddafe5
MAlexGG <alexandragalarzag@gmail.com>
2024-09-21 12:59:38 +0200

    saving a student with JPA and H2

--- src/main/java/com/ltp/gradesubmission/web/StudentController.java
+++ src/main/java/com/ltp/gradesubmission/web/StudentController.java
@@ -1,10 +1,12 @@
 package com.ltp.gradesubmission.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ltp.gradesubmission.entity.Student;
+import com.ltp.gradesubmission.service.StudentService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,12 +14,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
- 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/student")
 public class StudentController {
+
+    @Autowired
+    StudentService studentService;
 
     @GetMapping("/{id}")
     public ResponseEntity<Student> getStudent(@PathVariable Long id) {
@@ -25,9 +31,9 @@ public class StudentController {
     }
 
 
-    @PostMapping("/")
+    @PostMapping
     public ResponseEntity<Student> saveStudent(@RequestBody Student student) {
-        return new ResponseEntity<>(student, HttpStatus.CREATED);
+        return new ResponseEntity<>(studentService.saveStudent(student), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{id}")
